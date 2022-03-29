ac056c5da0f0de51d3090355984bb478be6a1d2a
hamisien <soboromilk@gmail.com>
2022-03-29 22:03:17 +0900

    Source code updated(Sockeet programming)

--- plang_old/network/sock_clnt.c
+++ plang_old/network/sock_clnt.c
@@ -9,27 +9,35 @@ void error_handling (const char* msg);
 
 int main(int argc, char* argv[])
 {
+    if (argc != 3) {
+        printf("Usage: %s <IP Address> <PORT>\n", argv[0]);
+        exit(1);
+    }
+
     int sock;
+    int read_byte;
+    struct sockaddr_in serv_addr;
+    char message[30] = {0,};
+
     sock = socket(PF_INET, SOCK_STREAM, 0);
     if (sock == -1)
         error_handling ("socket() err");
 
-    struct sockaddr_in serv_addr;
-
-    char message[30];
-
     memset(&serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = htonl(atoi(argv[1]));
+    serv_addr.sin_addr.s_addr = inet_addr(argv[1]); // inet_addr!!
     serv_addr.sin_port = htons(atoi(argv[2]));
 
-    if(connect(sock, (struct sockaddr*) &serv_addr, (socklen_t) sizeof(serv_addr)) == -1)
+    if(connect(sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) == -1)
         error_handling ("connect() err");
 
-    if(read(sock, message, sizeof(message)) == -1)
+    if((read_byte = read(sock, message, sizeof(message)+1)) == -1)
         error_handling ("read() err");
 
-    fputs(message, stdout);
+    printf("read_byte: %d\nMessage from server: %s \n ", read_byte, message);
+    // fputs(message, stdout);
+
+    close (sock);
 
     return 0;
 }
--- plang_old/network/sock_serv1.c
+++ plang_old/network/sock_serv1.c
@@ -12,21 +12,19 @@ int main(int argc, char* argv[])
     int sock_serv; /* socket() */
     int sock_clnt; /* accept() */
 
-    char message[] = "Hello, socket!";
+    struct sockaddr_in serv_addr;
+    struct sockaddr_in clnt_addr;
 
-    socklen_t clnt_addr_len;
+    char message[] = "Hello, socket!";
 
     if (argc != 2) {
         printf ("Usage: %s <PORT>\n", argv[0]);
         exit(1);
     }
 
-    sock_serv = socket(AF_INET, SOCK_STREAM, 0);
+    sock_serv = socket(PF_INET, SOCK_STREAM, 0);
     if (sock_serv == -1)
         error_handling ("socket() err");
-
-    struct sockaddr_in serv_addr;
-    struct sockaddr_in clnt_addr;
     
     memset(&serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
@@ -37,14 +35,16 @@ int main(int argc, char* argv[])
     if (bind(sock_serv, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) == -1)
         error_handling("bind() err");
 
-    if(listen(sock_serv, 5) != 0) 
+    if(listen(sock_serv, 5) == -1)
         error_handling ("listen() err");
+
+    socklen_t clnt_addr_size = sizeof(clnt_addr);
     
-    if((sock_clnt = accept(sock_serv, (struct sockaddr*) &clnt_addr, (socklen_t *) sizeof(clnt_addr))) == -1)
+    if(sock_clnt = accept(sock_serv, (struct sockaddr*) &clnt_addr, &clnt_addr_size) == -1)
         error_handling ("accept() err");
 
 
-    write (sock_serv, message, sizeof(message));
+    write (sock_clnt, message, sizeof(message));
 
     close (sock_clnt);
     close (sock_serv);
